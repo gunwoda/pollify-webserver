8e3ed8f09c67f9ec330dc21dadb87c5e9665d08a
gunwoda <95297035+gunwoda@users.noreply.github.com>
2023-06-10 22:03:37 +0900

    Navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,16 +3,16 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 const Header = styled.header`
-	z-index: 10;
-	width: 100%;
-	height: 60px;
-	display: flex;
-	align-items: center;
-	position: relative;
-	position: fixed;
-	top: 0;
-	border-bottom: 1px solid #d6d6d6;
-	background-color: white;
+  z-index: 10;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  position: fixed;
+  top: 0;
+  border-bottom: 1px solid #d6d6d6;
+  background-color: white;
 `;
 
 const Logo = styled.div`
@@ -35,12 +35,12 @@ const Menu = styled.div`
 `;
 
 const BtnBox = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	position: absolute;
-	right: 15%;
-	width: 150px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  right: 15%;
+  width: 150px;
 `;
 
 const LoginBtn = styled.button`
@@ -60,20 +60,26 @@ const SignUpBtn = styled.button`
 `;
 
 function Navbar() {
-	return (
-		<Header>
-			<Logo><Link to="/">Polify</Link></Logo>
-			<Nav>
-				<Menu><Link to="/">Home</Link></Menu>
-				<Menu><Link to="/createForm">Create Survey</Link></Menu>
-				<Menu><Link to="/QuerySurvey">All surveys</Link></Menu>
-			</Nav>
-			<BtnBox>
-				<Link to="/SignIn"><LoginBtn>Login</LoginBtn></Link>
-				<Link to="/SignUp"><SignUpBtn>SignUp</SignUpBtn></Link>
-			</BtnBox>
-		</Header>
-	);
+  // 로컬 스토리지에서 JWT 토큰 가져오기
+  const token = localStorage.getItem("jwtToken");
+
+  return (
+    <Header>
+      <Logo><Link to="/">Polify</Link></Logo>
+      <Nav>
+        <Menu><Link to="/">Home</Link></Menu>
+        <Menu><Link to="/createForm">Create Survey</Link></Menu>
+        <Menu><Link to="/QuerySurvey">All surveys</Link></Menu>
+      </Nav>
+      {!token && ( // 토큰이 없을 때만 BtnBox 보이도록 설정
+        <BtnBox>
+          <Link to="/SignIn"><LoginBtn>Login</LoginBtn></Link>
+          <Link to="/SignUp"><SignUpBtn>SignUp</SignUpBtn></Link>
+        </BtnBox>
+      )}
+    </Header>
+  );
 }
 
 export default Navbar;
+
